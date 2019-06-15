4c2921a183632a0ace6a33ad8baf01afdfab99de
efrat <efrat@Efrats-MacBook-Air.local>
2019-06-16 00:35:02 +0300

    remove explosion

--- src/js/content.js
+++ src/js/content.js
@@ -1,26 +1,12 @@
 import DragSelect from 'dragselect';
 import CursorMask from './CursorMask';
-function exlpodeElement(element){
-    return new Promise(res=>{
-        element.animate([
-            // keyframes
-            { transform: 'translateY(0px)' }, 
-            { transform: 'translateY(-300px)' }
-          ], { 
-            // timing options
-            duration: 1000,
-            iterations: Infinity
-          });
-    })
-}
 
-function setSelectorStyle(){
-    const elem = document.querySelector('.expand-drag-selector');
+function setSelectorStyle(selector){
     const r = Math.floor(Math.random() * (255 - 0 + 1)) ;
     const g = Math.floor(Math.random() * (255 - 0 + 1)) ;
     const b = Math.floor(Math.random() * (255 - 0 + 1)) ;
-    elem.style.background = `rgba(${r},${g},${b},0.1)`;
-    elem.style.border = `1px solid rgba(${r},${g},${b},1)`;
+    selector.style.background = `rgba(${r},${g},${b},0.1)`;
+    selector.style.border = `1px solid rgba(${r},${g},${b},1)`;
 }
 
 chrome.runtime.onMessage.addListener( (tab,obj,cb) => {
@@ -36,24 +22,22 @@ chrome.runtime.onMessage.addListener( (tab,obj,cb) => {
         selectedClass:'shake',
         autoScrollSpeed: 3
     });
-    setSelectorStyle();
+
+    const elem = document.querySelector('.expand-drag-selector');
+    setSelectorStyle(elem);
 
 function onselectEnd(selected) {
-  setTimeout(()=>{
-    cm.unmask();
-    ds.break();
-    // elem.parentNode && elem.parentNode.removeChild(elem);
     selected.forEach((link,index) =>{
         link.classList.remove('shake');
-        link.classList.add('explode');
     });
-    setTimeout(()=>{
+    cm.unmask();
+    ds.break();
+    elem.parentNode && elem.parentNode.removeChild(elem);
+  setTimeout(()=>{
         selected.forEach((link,index) =>{
-            link.classList.remove('explode');
             window.open(link.href, '_blank');
         });
-    },1000)
-  },500);
+    },800)
 }
     ds.callback = onselectEnd;
 });
